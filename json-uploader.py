906fd82e688ff767511008d96ac7ea815b31fc76
starseed777 <waress66@live.com>
2021-01-28 16:21:23 -0500

    json auto uploader code

---
+++ json-uploader.py
@@ -0,0 +1,17 @@
+from secrets import access_key , secret_access_key, bucket
+
+import boto3
+import os 
+
+client = boto3.client("s3",
+                        aws_access_key_id = access_key ,
+                        aws_secret_access_key = secret_access_key)
+
+for file in os.listdir():
+
+    if ".json" in file:
+        upload_bucket = bucket
+        upload_file = 'JSON/' +  str(file)
+        client.upload_file(file, upload_bucket, upload_file)
+        print("successfully uploaded all JSON files to s3")
+    
